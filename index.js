e16b5539cbafb59cb7b6f8111378e132e9b5cded
Jordan Scales <scalesjordan@gmail.com>
2016-04-09 17:50:17 -0400

    refactor save-index

--- index.js
+++ index.js
@@ -1,44 +1,8 @@
-var ejs = require("ejs")
-var fs = require("fs")
 var glob = require("glob")
 
+var saveIndex = require("./save-index.js")
 var loadArticle = require("./load-article.js")
 var saveArticle = require("./save-article.js")
-
-function saveIndex(articles) {
-    articles.sort((a, b) => {
-        return (b.rawDate || 0) - (a.rawDate || 0)
-    })
-
-    return new Promise((resolve, reject) => {
-        fs.readFile("templates/index.ejs", (err, data) => {
-            if (err) {
-                return reject(err)
-            }
-
-            var indexBodyHTML = ejs.render(data.toString(), {
-                articles: articles.filter((article) => {
-                    return !article.hidden
-                }),
-            })
-
-            fs.readFile("templates/layout.ejs", (err, data) => {
-                var indexHTML = ejs.render(data.toString(), {
-                    title: "jordan scales",
-                    body: indexBodyHTML,
-                })
-
-                fs.writeFile("build/index.html", indexHTML, (err) => {
-                    if (err) {
-                        return reject(err)
-                    }
-
-                    resolve("ok!")
-                })
-            })
-        })
-    })
-}
 
 glob("articles/*.md", (err, articles) => {
     if (err) {
---
+++ save-index.js
@@ -0,0 +1,39 @@
+var ejs = require("ejs")
+var fs = require("fs")
+
+function saveIndex(articles) {
+    articles.sort((a, b) => {
+        return (b.rawDate || 0) - (a.rawDate || 0)
+    })
+
+    return new Promise((resolve, reject) => {
+        fs.readFile("templates/index.ejs", (err, data) => {
+            if (err) {
+                return reject(err)
+            }
+
+            var indexBodyHTML = ejs.render(data.toString(), {
+                articles: articles.filter((article) => {
+                    return !article.hidden
+                }),
+            })
+
+            fs.readFile("templates/layout.ejs", (err, data) => {
+                var indexHTML = ejs.render(data.toString(), {
+                    title: "jordan scales",
+                    body: indexBodyHTML,
+                })
+
+                fs.writeFile("build/index.html", indexHTML, (err) => {
+                    if (err) {
+                        return reject(err)
+                    }
+
+                    resolve("ok!")
+                })
+            })
+        })
+    })
+}
+
+module.exports = saveIndex
