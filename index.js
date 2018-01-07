bd3be1b523a428afbefaf04f02a69c39945ed5a2
Jordan Scales <scalesjordan@gmail.com>
2018-01-07 11:31:22 -0500

    comment out 32x32 building for now

--- index.js
+++ index.js
@@ -8,39 +8,40 @@ var saveStaticFile = require("./save-static-file.js")
 var save32x32 = require("./save-32x32.js")
 
 glob("articles/*.md", (err, articles) => {
-    if (err) {
-        throw err
-    }
+  if (err) {
+    throw err
+  }
 
-    Promise
-        .all(articles.map(loadArticle))
-        .then((articles) => {
-            var promises = articles.map(saveArticle)
+  Promise.all(articles.map(loadArticle))
+    .then(articles => {
+      var promises = articles.map(saveArticle)
 
-            promises.push(saveIndex(articles, "output/index.html"))
-            promises.push(generateTags(articles))
+      promises.push(saveIndex(articles, "output/index.html"))
+      promises.push(generateTags(articles))
 
-            return Promise.all(promises)
-        })
-        .then(
-            () => console.log('Pages built.'),
-            (err) => {
-                console.log("Error building pages")
-                console.log(err)
-            })
+      return Promise.all(promises)
+    })
+    .then(
+      () => console.log("Pages built."),
+      err => {
+        console.log("Error building pages")
+        console.log(err)
+      }
+    )
 })
 
 glob("public/**/*", (err, staticFiles) => {
-    if (err) {
-        throw err
-    }
+  if (err) {
+    throw err
+  }
 
-    Promise.all(staticFiles.map(saveStaticFile))
-        .then(save32x32)
-        .then(
-            () => console.log('Static files saved.'),
-            (err) => {
-                console.log("Error saving static files")
-                console.log(err)
-            })
+  Promise.all(staticFiles.map(saveStaticFile))
+    //.then(save32x32)
+    .then(
+      () => console.log("Static files saved."),
+      err => {
+        console.log("Error saving static files")
+        console.log(err)
+      }
+    )
 })
