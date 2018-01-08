f58736f6db4cfa674ef51fd8f888bd03267599d3
Jordan Scales <scalesjordan@gmail.com>
2018-01-07 22:41:13 -0500

    add open graph meta tags

--- save-index.js
+++ save-index.js
@@ -2,39 +2,40 @@ var ejs = require("ejs")
 var fs = require("fs")
 
 function saveIndex(articles, outputFile, title, banner) {
-    articles.sort((a, b) => {
-        return (b.rawDate || 0) - (a.rawDate || 0)
-    })
+  articles.sort((a, b) => {
+    return (b.rawDate || 0) - (a.rawDate || 0)
+  })
 
-    return new Promise((resolve, reject) => {
-        fs.readFile("templates/index.ejs", (err, data) => {
-            if (err) {
-                return reject(err)
-            }
+  return new Promise((resolve, reject) => {
+    fs.readFile("templates/index.ejs", (err, data) => {
+      if (err) {
+        return reject(err)
+      }
 
-            var indexBodyHTML = ejs.render(data.toString(), {
-                articles: articles.filter((article) => {
-                    return !article.hidden
-                }),
-                banner,
-            })
+      var indexBodyHTML = ejs.render(data.toString(), {
+        articles: articles.filter(article => {
+          return !article.hidden
+        }),
+        banner,
+      })
 
-            fs.readFile("templates/layout.ejs", (err, data) => {
-                var indexHTML = ejs.render(data.toString(), {
-                    title: title || "jordan scales",
-                    body: indexBodyHTML,
-                })
-
-                fs.writeFile(outputFile, indexHTML, (err) => {
-                    if (err) {
-                        return reject(err)
-                    }
-
-                    resolve("ok!")
-                })
-            })
+      fs.readFile("templates/layout.ejs", (err, data) => {
+        var indexHTML = ejs.render(data.toString(), {
+          title: title || "jordan scales",
+          description: "jdan's thoughts and things",
+          body: indexBodyHTML,
         })
+
+        fs.writeFile(outputFile, indexHTML, err => {
+          if (err) {
+            return reject(err)
+          }
+
+          resolve("ok!")
+        })
+      })
     })
+  })
 }
 
 module.exports = saveIndex
