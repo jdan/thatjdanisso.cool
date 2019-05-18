f0e17ad1ec755c056c259ad2a325f4761d79c22a
Jordan Scales <scalesjordan@gmail.com>
2019-05-18 16:28:54 -0400

    add output to version control

---
+++ output/js/demos/clicking.js
@@ -0,0 +1,23 @@
+setInterval(function() {
+    var examples = [].slice.call(document.querySelectorAll(".a11y-onclick"));
+    var outputs = [].slice.call(
+        document.querySelectorAll(".a11y-onclick:not(.no-output) .output"));
+
+    examples.forEach(function(el) {
+        el.classList.add("active");
+    });
+
+    outputs.forEach(function(o) {
+        o.innerHTML = "Clicked!";
+    });
+
+    setTimeout(function() {
+        examples.forEach(function(el) {
+            el.classList.remove("active");
+        });
+
+        outputs.forEach(function(o) {
+            o.innerHTML = " ";
+        });
+    }, 1000);
+}, 2500);
---
+++ output/js/demos/hover.js
@@ -0,0 +1,41 @@
+var highlightIntervals = [
+    {
+        on: [550, 3900],
+        off: [1400, 4150]
+    },
+    {
+        on: [1700, 3500],
+        off: [1900, 3800]
+    },
+    {
+        on: [2000],
+        off: [3400]
+    }
+];
+
+function highlightSequence() {
+    var examples = [].slice.call(
+        document.querySelectorAll(".hover-a11y:not(.no-active)"));
+
+    examples.forEach(function(example) {
+        console.log(example)
+        var links = [].slice.call(example.querySelectorAll(".button"));
+
+        highlightIntervals.forEach(function(timers, i) {
+            timers.on.forEach(function(ms) {
+                setTimeout(function() {
+                    links[i].classList.add("active");
+                }, ms);
+            });
+
+            timers.off.forEach(function(ms) {
+                setTimeout(function() {
+                    links[i].classList.remove("active");
+                }, ms);
+            });
+        });
+    });
+}
+
+setInterval(highlightSequence, 6000);
+highlightSequence(); // call once to start
